26a7317ce7b402d7c57b21360cb0ecb4199cce12
Tyler <tyler.coy@wsu.edu>
2016-09-16 13:17:21 -0700

    Driver: Sage T. Navigator: Tyler C.
    Details: Renamed buttons and made a drop
    down menu. Also drew lines for grid.

--- main.java
+++ main.java
@@ -8,6 +8,11 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.BevelBorder;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
 
 public class Main extends JFrame {
 
@@ -28,19 +33,42 @@ public class Main extends JFrame {
 			}
 		});
 	}
-	public void paint(Graphics g){
+	
+	public void paint(Graphics g){//panel.getWidth
 		super.paint(g);
 		//put this in a for loop and make grid
-		g.drawLine(100, 0, 100, 300);//draw lines for the go board
-		g.setColor(Color.BLACK);//set the line colors to black
+		
+		
+		for(int i=0;i<=15;i++){
+			
+			int width=this.getWidth();
+			int height=this.getHeight();
+			
+			g.drawLine(0,i*height/20,15*width/20,i*height/20);//draw horizontal lines for the go board
+			g.drawLine(i*width/20,0,i*width/20,15*height/20);//draw vertical lines for the go board
+			g.setColor(Color.BLACK);//set the line colors to black
+		}
 	}
+	
 
 	/**
 	 * Create the frame.
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 510);
+		setBounds(100, 100, 506, 522);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnSystem = new JMenu("System");
+		menuBar.add(mnSystem);
+		
+		JMenuItem mntmChangeBackground = new JMenuItem("Change Background");
+		mnSystem.add(mntmChangeBackground);
+		
+		JMenuItem mntmQuit = new JMenuItem("Quit");
+		mnSystem.add(mntmQuit);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -48,18 +76,22 @@ public class Main extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.setBounds(10, 11, 464, 425);
+		panel.setBounds(20, 33, 438, 403);
 		contentPane.add(panel);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Restart");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton.setBounds(20, 437, 89, 23);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("New button");
+		JButton btnNewButton_1 = new JButton("Back");
 		btnNewButton_1.setBounds(184, 437, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("New button");
+		JButton btnNewButton_2 = new JButton("Quit");
 		btnNewButton_2.setBounds(359, 437, 89, 23);
 		contentPane.add(btnNewButton_2);
 	}
