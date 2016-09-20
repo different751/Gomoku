be3d41c0d2c7deaeb6a98e708a1b354426179782
Tyler <tyler.coy@wsu.edu>
2016-09-20 15:22:46 -0700

    Driver: Tyler Coy     Navigator: Sage T.   Added background drawing and fixed the drawing of lines

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-import java.awt.BorderLayout;
+mport java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
@@ -7,16 +7,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.BevelBorder;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
+import javax.swing.JTree;
 
 public class Main extends JFrame {
-
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -37,17 +40,29 @@ public class Main extends JFrame {
 	public void paint(Graphics g){//panel.getWidth
 		super.paint(g);
 		//put this in a for loop and make grid
+		File pope = new File("C:\\Users\\Tyler\\workspace\\Assignment2_pairprogramming\\src\\TNzMXds.jpg");
 		
+
+		try {
+			BufferedImage image=ImageIO.read(pope);
+			g.drawImage(image, 0,0,this.getWidth(),this.getHeight(), null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		for(int i=0;i<=15;i++){
 			
 			int width=this.getWidth();
 			int height=this.getHeight();
+			int hoffset=10+this.getWidth()/10;
+			int voffset=10+this.getHeight()/10;
 			
-			g.drawLine(0,i*height/20,15*width/20,i*height/20);//draw horizontal lines for the go board
-			g.drawLine(i*width/20,0,i*width/20,15*height/20);//draw vertical lines for the go board
+			g.drawLine(hoffset,i*height/20+voffset,15*width/20+hoffset,i*height/20+voffset);//draw horizontal lines for the go board
+			g.drawLine(i*width/20+hoffset,voffset,i*width/20+hoffset,15*height/20+voffset);//draw vertical lines for the go board
 			g.setColor(Color.BLACK);//set the line colors to black
 		}
+		
 	}
 	
 
@@ -55,6 +70,7 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 506, 522);
 		
@@ -69,31 +85,19 @@ public class Main extends JFrame {
 		
 		JMenuItem mntmQuit = new JMenuItem("Quit");
 		mnSystem.add(mntmQuit);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.setBounds(20, 33, 438, 403);
-		contentPane.add(panel);
+		JButton btnNewButton = new JButton("Back");
+		btnNewButton.setBounds(205, 438, 89, 23);
+		getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton = new JButton("Restart");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(20, 437, 89, 23);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Back");
-		btnNewButton_1.setBounds(184, 437, 89, 23);
-		contentPane.add(btnNewButton_1);
+		JButton btnNewButton_1 = new JButton("Restart");
+		btnNewButton_1.setBounds(106, 438, 89, 23);
+		getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Quit");
-		btnNewButton_2.setBounds(359, 437, 89, 23);
-		contentPane.add(btnNewButton_2);
+		btnNewButton_2.setBounds(304, 438, 89, 23);
+		getContentPane().add(btnNewButton_2);
+		
 	}
 }
-
